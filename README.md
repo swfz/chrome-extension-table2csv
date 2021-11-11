69367c10499639b3a8a1a6820d420c74ef5c99d7
swfz <sawafuji.09@gmail.com>
2021-11-11 13:34:02 +0900

    feat: いったん動くようになった

---
+++ README.md
@@ -0,0 +1,17 @@
+# Table to CSV Chrome Extension
+
+## Options
+
+
+- text input
+
+row selector
+column selectors[]
+
+- name
+
+これがpopupにリスト表示される
+
+- 
+
+
--- options.js
+++ options.js
@@ -21,6 +21,7 @@ function handleButtonClick(event) {
 
 // Add a button to the page for each supplied color
 function constructOptions(buttonColors) {
+
   chrome.storage.sync.get("color", (data) => {
     let currentColor = data.color;
     // For each color we were provided…
@@ -40,7 +41,56 @@ function constructOptions(buttonColors) {
       page.appendChild(button);
     }
   });
+
+  const saveInputs = () => {
+    const columnSelectors = document.querySelectorAll('input[name="columnSelectors"]');
+    const columnSelectorValues = Array.from(columnSelectors).map(input => input.value).filter(v => v !== '');
+
+    console.log(columnSelectorValues);
+
+    const rowSelector = document.querySelectorAll('input[name="rowSelector"]');
+    const rowSelectorValue = rowSelector.value;
+
+    chrome.storage.sync.set({rowSelector: rowSelectorValue, columnSelectors: columnSelectorValues});
+  }
+
+  const addColumnSelectorInput = (event, value = '') => {
+    console.log(value);
+    const columnSelectorsSpan = document.getElementById('columnSelectorsInput');
+
+    const columnSelectorsInputElem = document.createElement('input');
+    columnSelectorsInputElem.name = 'columnSelectors';
+    columnSelectorsInputElem.value = value;
+
+    columnSelectorsSpan.appendChild(columnSelectorsInputElem);
+    const br = document.createElement('br');
+    columnSelectorsSpan.appendChild(br);
+  }
+
+  chrome.storage.sync.get(['rowSelector', 'columnSelectors'], (options) => {
+    console.log(options);
+    // rowSelector
+    const rowSelectorSpan = document.getElementById('rowSelectorInput');
+
+    const rowSelectorInputElem = document.createElement('input');
+    rowSelectorInputElem.value = options.rowSelector;
+
+    rowSelectorSpan.appendChild(rowSelectorInputElem);
+
+    options.columnSelectors.forEach(columnSelector => {
+      addColumnSelectorInput(null, columnSelector);
+    });
+
+    // add selector event listener
+    const incrButton = document.getElementById('addColumnSelector');
+    incrButton.addEventListener('click', addColumnSelectorInput);
+
+    const saveButton = document.getElementById('saveSelector');
+    saveButton.addEventListener('click', saveInputs);
+  });
 }
 
 // Initialize the page by constructing the color options
 constructOptions(presetButtonColors);
+
+
